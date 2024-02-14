a23381e703af2f800b36ea36332d477fca9fc91a
Zakir Ufuk Sahiner <z.ufuk.sahiner@gmail.com>
2024-02-14 11:20:16 +0100

    Initial commit

---
+++ WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Zakir Ufuk Sahiner on 07.02.24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
